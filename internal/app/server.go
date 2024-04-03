e8cb2dfe03d9840a2e9dd9401de115027e3b4e04
WaffeSoul <firevampir98@gmail.com>
2024-04-03 15:05:20 -0400

    iter 1

---
+++ internal/app/server.go
@@ -0,0 +1,51 @@
+package app
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/WaffeSoul/metrics-collector/internal/storage"
+)
+
+func PostMetrics(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	data := strings.Split(r.URL.Path, "/")
+	if len(data) != 5 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	typeM := data[2]
+	nameM := data[3]
+	valueStrM := data[4]
+	switch typeM {
+	case "gauge":
+		valueM, err := strconv.ParseFloat(valueStrM, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		storage.StorageGause.Add(nameM, valueM)
+
+	case "counter":
+		valueM, err := strconv.ParseInt(valueStrM, 10, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		storage.StorageConter.Add(nameM, valueM)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+}
+
+func InitMux() (mux *http.ServeMux) {
+	mux = http.NewServeMux()
+	mux.HandleFunc("/update/", PostMetrics)
+	return
+}
---
+++ internal/storage/storage.go
@@ -0,0 +1,42 @@
+package storage
+
+var (
+	StorageGause  *Storage
+	StorageConter *Storage
+)
+
+type Storage struct {
+	items map[string]Item
+}
+
+type Item struct {
+	Value interface{}
+}
+
+func Init() *Storage {
+	items := make(map[string]Item)
+	return &Storage{
+		items: items,
+	}
+
+}
+
+func (s *Storage) Delete(key string) {
+
+}
+
+func (s *Storage) Add(key string, value interface{}) {
+	s.items[key] = Item{
+		Value: value,
+	}
+}
+
+func (s *Storage) Get(key string) (interface{}, bool) {
+	item, found := s.items[key]
+
+	// ключ не найден
+	if !found {
+		return nil, false
+	}
+	return item, true
+}

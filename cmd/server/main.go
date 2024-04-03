e2590f02331c1d61fd17b5477b9409ce03814cd5
WaffeSoul <firevampir98@gmail.com>
2024-04-03 14:13:15 -0400

    iter 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/WaffeSoul/metrics-collector/internal/app"
+	"github.com/WaffeSoul/metrics-collector/internal/storage"
+)
+
+func main() {
+	storage.StorageGause = storage.Init()
+	storage.StorageConter = storage.Init()
+	serve := app.InitMux()
+	err := http.ListenAndServe(`:8080`, serve)
+	if err != nil {
+		panic(err)
+	}
+}

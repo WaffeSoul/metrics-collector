5be52e508eb657263413e83b5f2bf3f934d88f2f
WaffeSoul <firevampir98@gmail.com>
2024-04-14 15:10:35 -0400

    fix agent

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -74,23 +74,30 @@ func (s *Collector) SendToServer(data string) error {
 
 func (s *Collector) UpdateMetricToServer() {
 	for {
+		var err error
 		s.mutex.Lock()
 		stringsMetric := s.fields.prepareSend()
 		counterStr := fmt.Sprintf("counter/PollCount/%d", s.counter)
 		randomStr := fmt.Sprintf("gauge/RandomValue/%f", rand.Float64())
 
 		for _, metric := range stringsMetric {
-			err := s.SendToServer(metric)
+			err = s.SendToServer(metric)
 			if err != nil {
-				continue
+				break
 			}
 		}
-		err := s.SendToServer(counterStr)
 		if err != nil {
+			s.mutex.Unlock()
+			continue
+		}
+		err = s.SendToServer(counterStr)
+		if err != nil {
+			s.mutex.Unlock()
 			continue
 		}
 		err = s.SendToServer(randomStr)
 		if err != nil {
+			s.mutex.Unlock()
 			continue
 		}
 		s.counter = 0

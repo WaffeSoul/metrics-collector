dae092b258783afd2e01a496398d81b88206b4d8
WaffeSoul <firevampir98@gmail.com>
2024-05-03 16:12:49 -0400

    Fix gzip

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -215,7 +215,7 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 
 func MiddlewareGzip(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Content-Encoding"), "gzip") and !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+		if !strings.Contains(r.Header.Get("Content-Encoding"), "gzip") && !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			next.ServeHTTP(w, r)
 			return
 		}
@@ -231,7 +231,7 @@ func MiddlewareGzip(next http.Handler) http.Handler {
 		}
 		r.Body = gzRead
 		w.Header().Set("Accept-Encoding", "gzip")
-		w.Header().Set("Content-Encoding", "gzip"
+		w.Header().Set("Content-Encoding", "gzip")
 		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gzWrite}, r)
 	})
 }
